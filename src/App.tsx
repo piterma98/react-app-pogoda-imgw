338203cc39c4dab9b4e63f8c9c7f26e6a4a1e736
piterma98 <piterma98@gmail.com>
2021-05-18 00:07:24 +0200

    routing start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
+import { Link, Route, Switch } from "react-router-dom";
+
+const Synoptic = () => (
+    <div>
+        <h2>Synoptic</h2>
+    </div>
+);
+
+const Hydrological = () => (
+    <div>
+        <h2>Hydrological</h2>
+    </div>
+);
+
+const Page404 = () => (
+    <div>
+        <h2>404</h2>
+    </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const getData=()=> {
+            fetch('https://danepubliczne.imgw.pl/api/data/synop')
+                .then(response => response.json())
+                .then(data => console.log(data));
+    };
+    getData();
+    return (
+        <div>
+            <nav className="navbar navbar-light">
+                <ul className="nav navbar-nav">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Synoptic">Synoptic data</Link>
+                    </li>
+                    <li>
+                        <Link to="/Hydrological">Hydrological data</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route exact path="/"><Home /></Route>
+                <Route path="/synoptic"><Synoptic /></Route>
+                <Route path="/Hydrological"><Hydrological /></Route>
+                <Route path="*" > <Page404/> </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = (props: any) => {
+    console.log(props);
+
+    return (
+        <div>
+            <h2>React app for fetching imgw data from api</h2>
+        </div>
+    );
+};
+
+export default Home;
